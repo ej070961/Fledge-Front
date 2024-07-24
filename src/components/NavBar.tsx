1330f6185672f4718fd43da9c7431e429660e929
yunseok <tjr6613@seoultech.ac.kr>
2024-07-24 12:14:34 +0900

    feat: NavBar UI 구현

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+import { ReactComponent as Logo } from "../assets/images/fledge.svg";
+import { ReactComponent as KakaoSignUp } from "../assets/images/kakao_sign_up.svg";
+import { ReactComponent as KaKaoSignIn } from "../assets/images/kakao_sign_in.svg";
+import tw from "twin.macro";
+import { useState } from "react";
+const NavBar = () => {
+    const [isSignUpHovered, setIsSignUpHovered] = useState(false);
+    const [isSignInHovered, setIsSignInHovered] = useState(false);
+    return (
+        <Container>
+            <MenuContainer>
+                <Logo />
+                <MenuItem>후원하기</MenuItem>
+                <MenuItem>챌린지</MenuItem>
+                <MenuItem>멘토링</MenuItem>
+                <MenuItem>정보공유</MenuItem>
+                <MenuItem>소개</MenuItem>
+            </MenuContainer>
+            <MenuContainer>
+                <SocialContainer
+                    onMouseEnter={() => setIsSignUpHovered(true)}
+                    onMouseLeave={() => setIsSignUpHovered(false)}
+                >
+                    <AuthItem>회원가입</AuthItem>
+                    {isSignUpHovered && (
+                        <SocialButton>
+                            <KakaoSignUp />
+                        </SocialButton>
+                    )}
+                </SocialContainer>
+                <SocialContainer
+                    onMouseEnter={() => setIsSignInHovered(true)}
+                    onMouseLeave={() => setIsSignInHovered(false)}
+                >
+                    <AuthItem>로그인</AuthItem>
+                    {isSignInHovered && (
+                        <SocialButton>
+                            <KaKaoSignIn />
+                        </SocialButton>
+                    )}
+                </SocialContainer>
+            </MenuContainer>
+        </Container>
+    );
+};
+
+export default NavBar;
+
+const SocialContainer = styled.div`
+    ${tw`
+        flex
+        flex-col
+        items-center
+        w-[85px]
+        cursor-pointer
+        relative
+    `}
+`;
+
+const SocialButton = styled.div`
+    position: absolute;
+    top: 30px;
+`;
+
+const Container = styled.div`
+    ${tw`
+        flex
+        justify-between
+        w-[1280px]
+        h-[85px]
+    `}
+`;
+
+const MenuContainer = styled.div`
+    ${tw`
+        flex
+        justify-center
+        items-center
+        gap-[15px]
+    `}
+`;
+
+const MenuItem = styled.div`
+    ${tw`
+        text-bold-20
+        text-center
+        text-fontColor1
+        w-[85px]
+        cursor-pointer
+    `}
+`;
+
+const AuthItem = styled.div`
+    ${tw`
+        text-medium-20
+        text-center
+        text-fontColor2
+    `}
+`;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import NavBar from "../components/NavBar";
+import styled from "styled-components";
 
 function Main() {
-  return <div>Main</div>;
+    return (
+        <Container>
+            <NavBar />
+            <div>Main</div>
+        </Container>
+    );
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
 
 export default Main;
