175102d668e93a468d547253e27946e1e81d5e5a
yunseok <tjr6613@seoultech.ac.kr>
2024-07-29 20:26:46 +0900

    feat: 마이페이지 상단 UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Sponsor from "./pages/Sponsor";
+import MyPage from "./pages/MyPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/sponsor" element={<Sponsor />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/sponsor" element={<Sponsor />} />
+                <Route path="/mypage" element={<MyPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
