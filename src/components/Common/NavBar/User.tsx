d68b584988cc6f35d8e3d640e677e6405cfc730f
yunseok <tjr6613@seoultech.ac.kr>
2024-08-07 21:37:02 +0900

    feat: 헤더 프로필, 로그아웃 링크

--- src/components/Common/NavBar/User.tsx
+++ src/components/Common/NavBar/User.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import tw from "twin.macro";
+import useAuthStore from "../../../storage/useAuthStore";
+import { useNavigate } from "react-router-dom";
 
 interface UserContainerProps {
     nickname: string;
@@ -7,13 +9,27 @@ interface UserContainerProps {
 }
 
 const User = ({ nickname, profile }: UserContainerProps) => {
+    const navigate = useNavigate();
+    const onLogout = () => {
+        useAuthStore.getState().logout();
+        navigate("/");
+    };
+    const onClickProfile = () => {
+        navigate("/mypage");
+    };
+
     return (
         <Container>
             <div>
                 <Nickname>{nickname}</Nickname> 님,
                 <span> 환영합니다!</span>
+                <Logout onClick={() => onLogout()}>로그아웃</Logout>
             </div>
-            <Profile src={profile} alt="profile" />
+            <Profile
+                src={profile}
+                alt="profile"
+                onClick={() => onClickProfile()}
+            />
         </Container>
     );
 };
@@ -33,7 +49,7 @@ const Container = styled.div`
 
 const Profile = styled.img`
     ${tw`
-        ml-[20px] w-[44px] h-[44px] rounded-full object-cover
+        ml-[17px] w-[44px] h-[44px] rounded-full object-cover cursor-pointer
     `}
 `;
 
@@ -42,5 +58,11 @@ const Nickname = styled.span`
         text-bold-20
         font-bold
         text-fontColor1
+    `}
+`;
+
+const Logout = styled.button`
+    ${tw`
+        ml-[18px] text-medium-20 font-medium text-fontColor2
     `}
 `;
