b1aed7c27f844821148fa165b3a06cd1fd2ea6f0
yunseok <tjr6613@seoultech.ac.kr>
2024-08-09 17:12:42 +0900

    feat: 메인 챌린지 섹션 수정

--- src/components/Challenge/ChainChallenge.tsx
+++ src/components/Challenge/ChainChallenge.tsx
@@ -1,28 +1,14 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 import ContentHeader from "../Common/ContentHeader";
-import LeftArrowIcon from "../../assets/icons/left-arrow";
-import { ChallengeItemLarge } from "./ChallengeItem";
-import RightArrowIcon from "../../assets/icons/right-arrow";
 import chain from "../../assets/images/challenge_chain.png";
-import { useState } from "react";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import { getPartnershipChallenges } from "../../apis/challenge";
+import ChainChallengeList from "./ChainChallengeList";
 
-const ChainChallenge = () => {
-    const [page, setPage] = useState<number>(0);
+export type ChallengeGridProps = {
+    onePage?: boolean;
+};
 
-    const { data: challengeData, isLoading } = useQuery({
-        queryKey: ["getPartnershipChallenges", page],
-        queryFn: () => getPartnershipChallenges(page, 2),
-        enabled: true,
-        placeholderData: keepPreviousData,
-    });
-
-    if (isLoading) return <div></div>;
-
-    console.log(challengeData);
-
+const ChainChallenge = ({ onePage }: ChallengeGridProps) => {
     return (
         <Container>
             <ContentHeader
@@ -30,60 +16,15 @@ const ChainChallenge = () => {
                 desc="정부나 전담기관 지원 챌린지, 기업 파트너십 챌린지에 도전해보세요!"
                 imgSrc={chain}
             />
-            <ChallengerContainer>
-                <button onClick={() => setPage(page - 1)} disabled={page === 0}>
-                    <LeftArrowIcon width={24} height={51} />
-                </button>
-                <ChallengeSlider>
-                    {challengeData.data.content.map(
-                        (challenge: any, index: number) => (
-                            <ChallengeItemLarge
-                                key={index}
-                                title={challenge.title}
-                                bubbleType={challenge.type}
-                                heartCount={challenge.likeCount}
-                                challengeTypes={challenge.categories}
-                                description={challenge.description}
-                                successRate={challenge.successRate}
-                                participants={challenge.participantCount}
-                                startDate={challenge.startDate}
-                                endDate={challenge.endDate}
-                                supportContent={challenge.supportContent}
-                            />
-                        )
-                    )}
-                </ChallengeSlider>
-                <button
-                    onClick={() => setPage(page + 1)}
-                    disabled={challengeData.data.last}
-                >
-                    <RightArrowIcon width={24} height={51} />
-                </button>
-            </ChallengerContainer>
+            <ChainChallengeList onePage={onePage} />
         </Container>
     );
 };
 
 export default ChainChallenge;
 
-const ChallengerContainer = styled.div`
-    ${tw`
-        flex
-        items-center
-        justify-between
-        gap-[40px]
-        mt-[-170px]
-    `}
-`;
-
 const Container = styled.div`
     ${tw`
         mt-[50px] flex flex-col items-center relative w-full
     `}
-`;
-
-const ChallengeSlider = styled.div`
-    ${tw`
-    grid grid-cols-2  gap-[23px]
-`}
 `;
--- src/components/Challenge/ChainChallengeList.tsx
+++ src/components/Challenge/ChainChallengeList.tsx
@@ -1,46 +1,26 @@
-import { useEffect, useState } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import LeftArrowIcon from "../../assets/icons/left-arrow";
-import { ChallengeItem } from "./ChallengeItem";
+import { ChallengeItemLarge } from "./ChallengeItem";
 import RightArrowIcon from "../../assets/icons/right-arrow";
-import { getChallenges } from "../../apis/challenge";
+import { useState } from "react";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import { on } from "events";
+import { getPartnershipChallenges } from "../../apis/challenge";
+import { ChallengeGridProps } from "./ChainChallenge";
 
-type ChallengeGridProps = {
-    type: string;
-    categories?: string[];
-    size?: number;
-    onePage?: boolean;
-};
-
-const ChallengeGrid = ({
-    type,
-    categories,
-    size,
-    onePage = false,
-}: ChallengeGridProps) => {
+const ChainChallengeList = ({ onePage = false }: ChallengeGridProps) => {
     const [page, setPage] = useState<number>(0);
-    const isCategory = categories !== undefined;
-    const {
-        data: challengeData,
-        isLoading,
-        error,
-    } = useQuery({
-        queryKey: ["getChallengeData", page, type, categories || []],
-        queryFn: () => getChallenges(page, size || 8, type, categories || []),
+
+    const { data: challengeData, isLoading } = useQuery({
+        queryKey: ["getPartnershipChallenges", page],
+        queryFn: () => getPartnershipChallenges(page, 2),
         enabled: true,
         placeholderData: keepPreviousData,
     });
 
-    useEffect(() => {
-        setPage(0);
-    }, [categories]);
-
     if (isLoading) return <div></div>;
-    if (challengeData === undefined) return <div></div>;
 
+    console.log(challengeData);
     return (
         <ChallengerContainer>
             {!onePage && (
@@ -51,16 +31,18 @@ const ChallengeGrid = ({
             <ChallengeSlider>
                 {challengeData.data.content.map(
                     (challenge: any, index: number) => (
-                        <ChallengeItem
+                        <ChallengeItemLarge
                             key={index}
                             title={challenge.title}
-                            bubbleType={type.toString()}
+                            bubbleType={challenge.type}
                             heartCount={challenge.likeCount}
                             challengeTypes={challenge.categories}
                             description={challenge.description}
                             successRate={challenge.successRate}
                             participants={challenge.participantCount}
-                            isCategory={isCategory}
+                            startDate={challenge.startDate}
+                            endDate={challenge.endDate}
+                            supportContent={challenge.supportContent}
                         />
                     )
                 )}
@@ -76,7 +58,8 @@ const ChallengeGrid = ({
         </ChallengerContainer>
     );
 };
-export default ChallengeGrid;
+
+export default ChainChallengeList;
 
 const ChallengerContainer = styled.div`
     ${tw`
@@ -84,13 +67,12 @@ const ChallengerContainer = styled.div`
         items-center
         justify-between
         gap-[40px]
-        mt-[-80px]
-        w-[1400px]
+        mt-[-170px]
     `}
 `;
 
 const ChallengeSlider = styled.div`
     ${tw`
-    grid grid-cols-4 grid-rows-2 gap-[23px]
+    grid grid-cols-2  gap-[23px]
 `}
 `;
